db937aafacfa7962adb2dbd42986cf7d81fe05e2
xenith311-stack <xenith311@gmail.com>
2025-11-03 15:05:49 +0530

    Delete src directory

--- src/components/VantaBackground.tsx
+++
@@ -1,6 +0,0 @@
-import "./VantaBackground.css";
-
-export default function VantaBackground({ variant = 'fixed' as 'fixed' | 'local' }) {
-  return <div className={variant === 'fixed' ? "vanta-background" : "vanta-background-local"}></div>;
-}
-
